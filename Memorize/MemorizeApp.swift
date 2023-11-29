af3938ceb3c809e02312e8749aa623e31be9de54
wangyichao <im.wangyichao@qq.com>
2023-11-29 21:05:27 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by wangyichao on 2023/11/29.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
